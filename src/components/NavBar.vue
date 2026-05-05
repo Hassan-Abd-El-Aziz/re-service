7aa75238a8952abf9cd8af920fe44366eb85542c
Hassan Abd El Aziz <hassanabdelaziz940@gmail.com>
2026-05-05 15:49:51 +0300

    add mobile menu in navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -15,7 +15,6 @@ flex items-center justify-between">
 </div>
 
 <!-- links -->
- <div>
     <ul class="hidden md:flex items-center gap-6 lg:gap-8
      text-gray-700 font-medium">
 <li v-for="item in menuItems" :key="item.name"
@@ -23,13 +22,52 @@ class="hover:text-green-500 cursor-pointer transition-colors">
 
 <a :href="item.path">{{ item.name }}</a></li>
     </ul>
+
+    <!-- button -->
+    <div class="hidden md:block">
+        <button class="px-4 py-2 sm:px-5 sm:py-2 rounded-lg
+         bg-pink-500 text-white font-medium hover:bg-pink-600 
+         transition-colors">
+Get Started
+        </button>
+    </div>
+    <!-- mobile menu icon -->
+     <div class="md:hidden ">
+<button @click="toggleMenu" 
+class="p-1 rounded-md focus:outline-none focus:ring-2
+ focus:ring-pink-500">
+<component :is="isMenuOpen ? X : Menu"
+ class="h-6 w-6 text-gray-700"/>
+</button>
+     </div>
  </div>
+
+    <!-- mobile menu  -->
+<div v-if="isMenuOpen"
+class="md:hidden bg-white shadow-lg border-t border-gray-200">
+<div class="px-4 py-3 space-y-3">
+    <a :href="item.path" v-for="item in menuItems" :key="item.name"
+    class="block py-2 px-4 text-gray-700 hover:bg-gray-100 rounded-lg
+     hover:text-green-500 transition-colors "> {{ item.name }}</a>
+     <div class="pt-2 ">
+<button class="w-full py-2 rounded-lg bg-pink-500
+ text-white font-medium hover:bg-pink-600 transition-colors">
+Get Started</button>
+     </div>
 </div>
+</div>
+
 </nav>
 </template>
 
 <script setup>
+import { ref } from 'vue';
+import {X,Menu} from 'lucide-vue-next';
 
+const isMenuOpen=ref(false);
+const toggleMenu=()=>{
+    isMenuOpen.value=!isMenuOpen.value;
+}
 const menuItems=[
     {name:"Home",path:"#"},
     {name:"About",path:"#about"},
