69446026a9a88de3502bdaeecd334e891ac725b7
Hassan Abd El Aziz <hassanabdelaziz940@gmail.com>
2026-06-01 16:21:52 +0300

    start about secton

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
     <div>
         <NavBar />
         <Hero />
+        <About />
     </div>
 </template>
 <script setup>
 import { onMounted } from 'vue';
 import NavBar from './components/NavBar.vue';
 import Hero from './components/Hero.vue';
+import About from './components/About.vue';
 
 import AOS from 'aos';
 import 'aos/dist/aos.css';
---
+++ src/components/About.vue
@@ -0,0 +1,16 @@
+<template>
+    <section id="about" class="relative overflow-hidden bg-gradient-to-br from-gray-50 
+    to-green-50 py-12 px-4 sm:py-16 md:px-12 lg:px-20 flex
+    flex-col lg:flex-row items-center justify-between ">
+        <div class="flex w-full lg:mr-8 xl:mr-12 relative order-2 
+        lg:order-1 mt-10 lg:mt-0">
+            <div class="w-full max-w-md mx-auto lg:max-w-lg xl:max-w-xl
+    h-[300px] sm:h-[350px] md:h-[400px] lg:h-[450px] xl:h-[500px]
+     overflow-hidden shadow-lg md:shadow-xl">
+                <img src="../assets/2.png" alt="">
+            </div>
+        </div>
+    </section>
+</template>
+<script setup>
+</script>
